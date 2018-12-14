c8be68441f272c642e7f2d720a8190b532554222
Sindhu40 <45780564+Sindhu40@users.noreply.github.com>
2018-12-14 15:35:39 +0530

    Update alphabet.py

--- alphabet.py
+++ alphabet.py
@@ -6,8 +6,8 @@ int main()
     printf("Enter a character: ");
     scanf("%c",&c);
    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
-         printf("%c is an alphabet",c);
+         printf("this is an alphabet",c);
    else:
-         printf("%c is not an alphabet",c); 
+         printf("this is not an alphabet",c); 
          return 0;
 }
