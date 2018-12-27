89389b64a19537e1ff917bb696b6ce426d06c541
Sindhu40 <45780564+Sindhu40@users.noreply.github.com>
2018-12-27 15:22:04 +0530

    Create n time.py

---
+++ n time.py
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i, n;
+  cin >> n;
+  for (i=0; i<n; i++) 
+   {
+     cout << "hello world";
+   }
+   return 0;
+}
